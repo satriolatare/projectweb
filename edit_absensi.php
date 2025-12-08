a42aef42d22435cd675a38be508520366ad9dd3f
satriolatare <mohammadsatrio06@gmail.com>
2025-12-09 02:24:35 +0800

    update edit

--- edit_absensi.php
+++ edit_absensi.php
@@ -2,8 +2,6 @@
 
 <?php
 $id_absensi = $_GET['id'];
-$siswa = $conn->query("SELECT * FROM siswa");
-$mapel = $conn->query("SELECT * FROM mapel");
 
 if (isset($id_absensi)) {
     $sql = "SELECT * FROM absen WHERE id_absensi='$id_absensi'";
@@ -16,6 +14,13 @@ if (isset($id_absensi)) {
     }
     $nilai = $result->fetch_assoc();
 }
+
+// ❗ PERBAIKAN PENTING: ambil siswa sesuai kelas
+$siswa = $conn->query("SELECT * FROM siswa WHERE id_kelas
+= '{$nilai['id_kelas']}'");
+
+$mapel = $conn->query("SELECT * FROM mapel");
+
 if ($_POST) {
 
     $id_siswa = $_POST["id_siswa"];
@@ -23,11 +28,10 @@ if ($_POST) {
     $tanggal  = $_POST["tanggal"];
     $status   = $_POST["status"];
 
-    // Ambil kelas dari siswa (biar sama kayak simpan_absensi)
+    // Ambil kelas dari siswa
     $q    = $conn->query("SELECT id_kelas FROM siswa WHERE id_siswa = '$id_siswa'");
     $data = $q->fetch_assoc();
     $id_kelas = $data["id_kelas"];
-
     
     $sql_update = "
         UPDATE absen SET 
@@ -49,12 +53,10 @@ if ($_POST) {
 }
 ?>
 
-
 <div class="container">
     <form action="" method="POST">
         <div class="row">
 
-        
             <div class="col-md-4">
                 <label>Nama Siswa</label>
                 <select class="form-control" name="id_siswa" id="id_siswa" required>
@@ -69,7 +71,6 @@ if ($_POST) {
                 </select>
             </div>
 
-    
             <div class="col-md-4">
                 <label>Mata Pelajaran</label>
                 <select class="form-control" name="id_mapel" id="id_mapel" required>
@@ -83,7 +84,6 @@ if ($_POST) {
                 </select>
             </div>
 
-            
             <div class="col-md-4">
                 <label>Tanggal</label>
                 <input 
@@ -95,7 +95,6 @@ if ($_POST) {
                     required>
             </div>
 
-            
             <div class="col-md-4 mt-3">
                 <label>Status</label>
                 <select class="form-control" name="status" id="status" required>
